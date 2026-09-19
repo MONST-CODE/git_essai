4408675c7553532942a982f5ed4716c1b06f3713
MONST-CODE <edinzambabruno@gmail.com>
2026-09-19 20:51:49 +0100

    ajout de la partie salutations

---
+++ commmit.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+int main() {
+    //Salutations
+    std::cout << "Hello, World!" << std::endl;
+    std::cout<<"Entrer votre Nom "<<std::endl;
+    std::string nom;
+    std::cin>>nom;
+    std::cout<<"bonjour "<<nom<<std::endl;
+    
+    
+    
+    
+    
+    return 0;
+}
